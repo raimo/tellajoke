5dbc40527d6b63a02275b77f037884f536366aef
raimo-t <rtuisku@niksula.hut.fi>
2010-10-17 00:55:39 +0300

    Fix postgres problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def fetch_hotlist
-    @best_jokes = Joke.all(:conditions => ['created_at BETWEEN ? AND ?', Date.today.beginning_of_week, Date.today.end_of_week], :order => 'points desc')
-    @new_jokes = Joke.all(:conditions => ['created_at BETWEEN ? AND ?', Date.today.beginning_of_week, Date.today.end_of_week])
+    @best_jokes = Joke.all(:conditions => ['created_at BETWEEN ? AND ?', Date.today.beginning_of_week, Date.today.end_of_week], :order => 'points desc', :limit => Joke::MAX_COUNT)
+    @new_jokes = Joke.all(:conditions => ['created_at BETWEEN ? AND ?', Date.today.beginning_of_week, Date.today.end_of_week], :order => 'created_at desc', :limit => Joke::MAX_COUNT)
     @week = Date.today.cweek
   end
 end
