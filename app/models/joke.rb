aeacba093efb5d4236a8dfca1c8f5f68fc31ae8d
raimo-t <rtuisku@niksula.hut.fi>
2010-10-16 17:30:13 +0300

    add nice URLs

--- app/models/joke.rb
+++ app/models/joke.rb
@@ -7,4 +7,8 @@ class Joke < ActiveRecord::Base
   validates_presence_of :body
   attr_protected :points, :user_id
 
+  def to_param
+    "#{self.id}-#{body[0..40].gsub(/[^a-zA-Z_ ]/, '').gsub(' ','-')}"
+  end
+
 end
