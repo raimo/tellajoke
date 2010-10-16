a0985aed4e5864a535a41ef85cfe21de78bbf567
raimo-t <rtuisku@niksula.hut.fi>
2010-10-16 04:55:52 +0300

    ds

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,20 @@
 set :application, "tellajoke"
-set :repository,  "http://github.com/railsrumble/rr10-team-311"
+set :repository,  "git@github.com:raimo-t/tellajoke.git"
 
-set :scm_username, 'tellajoke'
-set :scm_password, 'telljuhaniapetrijoke'
+set :branch, "master"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
+set :user, 'tellajoke'
+set :password, 'telljuhaniapetrijoke'
+
+set :use_sudo, false
 set :deploy_to, "/home/#{application}"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
 set :scm, :git
 
 role :app, "173.255.195.81"
 role :web, "173.255.195.81"
 role :db,  "173.255.195.81", :primary => true
+
+
+task :restart, :roles => :app, :except => { :no_release => true } do
+  run "touch #{current_path}/tmp/restart.txt"
+end
