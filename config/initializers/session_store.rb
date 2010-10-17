2a870d015cbe38b9e924532c38687fa41398bebc
raimo-t <rtuisku@niksula.hut.fi>
2010-10-17 16:59:26 +0300

    Added sessions in db

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ Tellajoke::Application.config.session_store :cookie_store, :key => '_tellajoke_s
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# Tellajoke::Application.config.session_store :active_record_store
+Tellajoke::Application.config.session_store :active_record_store
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101017110649) do
+ActiveRecord::Schema.define(:version => 20101017135831) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(:version => 20101017110649) do
     t.datetime "updated_at"
     t.string   "teller"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "votes", :force => true do |t|
     t.string   "ip"
